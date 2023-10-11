fe2501a180ac8d21d6f0cd1f171897d3a7f9a329
hiennh <thanhace2000@gmail.com>
2023-10-11 14:54:25 +0700

    First commit

---
+++ src/main/java/com/hien/food/HienFoodApplication.java
@@ -0,0 +1,13 @@
+package com.hien.food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HienFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HienFoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hien/food/HienFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hien.food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HienFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
